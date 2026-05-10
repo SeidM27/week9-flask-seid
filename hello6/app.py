709f8f3663c317c5c4af8b4569734d5ce9596a99
SeidM27 <seidmamuti1313@gmail.com>
2026-05-10 14:02:37 +0200

    Update app.py
    Seid Mamuti week 9 assignment

--- hello6/app.py
+++ hello6/app.py
@@ -13,3 +13,32 @@ def index():
 @app.route("/greet")
 def greet():
     return render_template("greet.html", name=request.args.get("name", "world"))
+    ello6/app.py
+from flask import Flask, render_template, request
+app = Flask(__name__)
+@app.route("/")
+def index():
+return render_template("index.html")
+@app.route("/greet")
+def greet():
+name = request.args.get("name", "world")
+return render_template("greet.html", name=name)
+<!DOCTYPE html>
+<html lang="en">
+<head><title>Hello</title></head>
+<body>
+<form action="/greet" method="get">
+<input autocomplete="off" autofocus name="name" placeholder="Name" type="text">
+<button type="submit">Greet</button>
+</form>
+</body>
+</html>
+hello6/templates/greet.html
+<!DOCTYPE html>
+<html lang="en">
+<head><title>Greet</title></head>
+<body>
+<h1>hello, {{ name }}</h1>
+</body>
+</html>
+
