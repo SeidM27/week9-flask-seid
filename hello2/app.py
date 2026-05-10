2fd67fa4ff274167945d1bf323cdede45fa98f57
SeidM27 <seidmamuti1313@gmail.com>
2026-05-10 13:53:21 +0200

    Fix duplicate index route in app.py
    Seid Mamuti week 9 assignment

--- hello2/app.py
+++ hello2/app.py
@@ -8,3 +8,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route("/")
+def index():
+return render_template("index.html")
