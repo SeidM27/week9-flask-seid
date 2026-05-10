1384793124ddb29cadec27a18101e49156d8fae7
SeidM27 <seidmamuti1313@gmail.com>
2026-05-10 14:05:30 +0200

    Update app.py
    Seid Mamuti week 9 assignment

--- hello8/app.py
+++ hello8/app.py
@@ -13,3 +13,29 @@ def index():
 @app.route("/greet", methods=["POST"])
 def greet():
     return render_template("greet.html", name=request.form.get("name", "world"))
+from flask import Flask, render_template, request
+app = Flask(__name__)
+@app.route("/")
+def index():
+return render_template("index.html")
+@app.route("/greet", methods=["POST"])
+def greet():
+name = request.form.get("name", "world")
+return render_template("greet.html", name=name)
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Flask Exercise</title>
+</head>
+<body>
+{% block body %}{% endblock %}
+</body>
+</html>
+             {% block body %}
+<form action="/greet" method="post">
+<input autocomplete="off" autofocus name="name" placeholder="Name" type="text">
+<button type="submit">Greet</button>
+</form>
+{% endblock %}
+{% block body %}
