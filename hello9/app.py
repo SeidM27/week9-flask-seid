304c3b92b3bcc77b86e23402834390cd86954acf
SeidM27 <seidmamuti1313@gmail.com>
2026-05-10 14:06:48 +0200

    Update app.py
    Seid Mamuti week 9 assignment

--- hello9/app.py
+++ hello9/app.py
@@ -10,3 +10,27 @@ def index():
     if request.method == "POST":
         return render_template("greet.html", name=request.form.get("name", "world"))
     return render_template("index.html")
+from flask import Flask, render_template, request
+app = Flask(__name__)
+@app.route("/", methods=["GET", "POST"])
+def index():
+if request.method == "POST":
+name = request.form.get("name", "world")
+return render_template("greet.html", name=name)
+return render_template("index.html")
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Flask Exercise</title>
+</head>
+<body>
+{% block body %}{% endblock %}
+</body>
+</html>
+{% block body %}
+<form action="/" method="post">
+<input autocomplete="off" autofocus name="name" placeholder="Name" type="text">
+<button type="submit">Greet</button>
+</form>
+{% endblock %}
