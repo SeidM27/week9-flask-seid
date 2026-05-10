591221caa8661488a405e0bd9ac040956e96718f
SeidM27 <seidmamuti1313@gmail.com>
2026-05-10 13:54:19 +0200

    Refactor Flask app to improve index route handling
    Seid Mamuti week 9 assignment

--- hello3/app.py
+++ hello3/app.py
@@ -12,3 +12,16 @@ def index():
     else:
         name = "world"
     return render_template("index.html", placeholder=name)
+from flask import Flask, render_template, request
+app = Flask(__name__)
+@app.route("/")
+def index():
+name = request.args.get("name")
+return render_template("index.html", name=name)
+!DOCTYPE html>
+<html lang="en">
+<head><title>Hello</title></head>
+<body>
+<h1>hello, {{ name }}</h1>
+</body>
+</html>
