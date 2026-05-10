f8092767b5e5b00d4c50f3ac2155d6c5ebfa7144
SeidM27 <seidmamuti1313@gmail.com>
2026-05-10 14:03:39 +0200

    Update app.py
    Seid Mamuti Week 9

--- hello7/app.py
+++ hello7/app.py
@@ -13,3 +13,22 @@ def index():
 @app.route("/greet")
 def greet():
     return render_template("greet.html", name=request.args.get("name", "world"))
+from flask import Flask, render_template, request
+app = Flask(__name__)
+@app.route("/")
+def index():
+return render_template("index.html")
+@app.route("/greet")
+def greet():
+name = request.args.get("name", "world")
+return render_template("greet.html", name=name)
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Flask Exercise</title>
+</head>
+<body>
+{% block body %}{% endblock %}
+</body>
+</html>
