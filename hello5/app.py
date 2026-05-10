81126fbb297ad8fe4ac4d50ff68c9a0a2afb3e0c
SeidM27 <seidmamuti1313@gmail.com>
2026-05-10 14:01:37 +0200

    Update app.py
    Seid Mamuti week 9 assignment

--- hello5/app.py
+++ hello5/app.py
@@ -9,3 +9,16 @@ app = Flask(__name__)
 def index():
     name = request.args.get("name", "world")
     return render_template("index.html", name=name)
+    from flask import Flask, render_template, request
+app = Flask(__name__)
+@app.route("/")
+def index():
+name = request.args.get("name", "world")
+return render_template("index.html", name=name)
+<!DOCTYPE html>
+<html lang="en">
+<head><title>Hello</title></head>
+<body>
+<h1>hello, {{ name }}</h1>
+</body>
+</html>
