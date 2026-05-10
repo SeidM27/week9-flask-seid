1e5ced6b6134ee3c17da9230822050a60bc97e37
SeidM27 <seidmamuti1313@gmail.com>
2026-05-10 14:14:52 +0200

    Update app.py
    Seid Mamuti week 9 assignment

--- froshims3/app.py
+++ froshims3/app.py
@@ -17,3 +17,18 @@
 #         2. Get "sport" — if missing, render error.html with message="Missing sport"
 #         3. If sport is not in SPORTS, render error.html with message="Invalid sport"
 #         4. If all valid, render success.html
+from flask import Flask, render_template, request
+app = Flask(__name__)
+SPORTS = ["Basketball", "Football", "Volleyball", "Tennis"]
+@app.route("/")
+def index():
+return render_template("index.html", sports=SPORTS)
+@app.route("/register", methods=["POST"])
+def register():
+name = request.form.get("name")
+sport = request.form.get("sport")
+if not name:
+return render_template("error.html", message="Missing name")
+if sport not in SPORTS:
+return render_template("error.html", message="Invalid sport")
+return render_template("success.html")
