4e3735f4d1bdf482d01715695431964dababb642
SeidM27 <seidmamuti1313@gmail.com>
2026-05-10 14:12:45 +0200

    Implement registration form with Flask
    Seid Mamuti week 9 assignment

--- froshims1/app.py
+++ froshims1/app.py
@@ -18,3 +18,30 @@
 #         - Reads "sport" from the form
 #         - If name is missing OR sport is not in the SPORTS list → render failure.html
 #         - Otherwise → render success.html
+from flask import Flask, render_template, request
+app = Flask(__name__)
+SPORTS = ["Basketball", "Football", "Volleyball", "Tennis"]
+@app.route("/")
+def index():
+return render_template("index.html", sports=SPORTS)
+@app.route("/register", methods=["POST"])
+def register():
+name = request.form.get("name")
+sport = request.form.get("sport")
+if not name or sport not in SPORTS:
+return render_template("failure.html")
+return render_template("success.html")
+{% block body %}
+<h1>Register</h1>
+<form action="/register" method="post">
+<input autocomplete="off" autofocus name="name" placeholder="Name" type="text">
+<select name="sport">
+<option value="">Sport</option>
+{% for sport in sports %}
+<option value="{{ sport }}">{{ sport }}</option>
+{% endfor %}
+</select>
+<button type="submit">Register</button>
+</form>
+{% endblock %}
+
